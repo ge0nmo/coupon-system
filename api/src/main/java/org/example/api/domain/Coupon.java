28858281295bd7ac13af9b1a1ae8e721d35703c3
ge0nmo <ge0nmo@naver.com>
2024-04-20 16:06:46 +0900

    first commit

---
+++ api/src/main/java/org/example/api/domain/Coupon.java
@@ -0,0 +1,27 @@
+package org.example.api.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Coupon
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    public Coupon(Long userId)
+    {
+        this.userId = userId;
+    }
+}
---
+++ api/src/test/java/org/example/api/ApiApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
